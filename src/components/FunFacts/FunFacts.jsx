58c312baea8e582fd1e80de26968a238c128fd63
Mamareza <mamareza@Mamarezas-MacBook-Air.local>
2022-11-18 02:24:33 +0330

    news

--- src/components/FunFacts/FunFacts.jsx
+++ src/components/FunFacts/FunFacts.jsx
@@ -1,6 +1,30 @@
 import mapImg from "../../assets/img/map.png"
+import useAxiosFunction from "../../axiosFetch/useAxiosFunction"
+import axios from "../../apis/axiosBase"
+import { useEffect } from "react"
+import { useState } from "react"
+
 
 const FunFacts = () =>{
+    const [posts, error, loading, axiosFetch] = useAxiosFunction()
+
+    
+    const getStatistics = () =>{
+        axiosFetch({
+            axiosInstance : axios,
+            method : 'get',
+            url : '/statistics'
+        })
+
+    }
+
+    useEffect(()=>{
+        getStatistics()
+
+        // eslint-disable-next-line 
+    },[])
+
+
     return(
         <section className="funfacts-area ptb-110 bg-fcfbfb">
         <div className="container">
@@ -11,14 +35,14 @@ const FunFacts = () =>{
             <div className="row">
                 <div className="col-lg-6 col-md-6 col-sm-6 col-6">
                     <div className="funfact">
-                        <h3><span style={{direction:"ltr"}} className="odometer" data-count="180">00</span>+</h3>
+                        <h3><span style={{direction:"ltr"}} className="odometer" data-count="180">{loading && "0"}{!loading && error && "خطا"}{!loading && !error && posts.data?.number_customer}</span>+</h3>
                         <p>تعداد مشتری</p>
                     </div>
                 </div>
     
                 <div className="col-lg-6 col-md-6 col-sm-6 col-6">
                     <div className="funfact">
-                        <h3><span style={{direction:"ltr"}} className="odometer" data-count="20">00</span>+</h3>
+                        <h3><span style={{direction:"ltr"}} className="odometer" data-count="20">{posts.data?.number_funds}</span>+</h3>
                         <p>تعداد صندوق ها</p>
                     </div>
                 </div>
--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,141 +1,73 @@
+import useAxiosFunction from "../../axiosFetch/useAxiosFunction";
+import axios from "../../apis/axiosBase";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
+const News = () => {
+  const [posts, error, loading, axiosFetch] = useAxiosFunction();
 
-const News = () =>{
-    return(
-        <section className="blog-area ptb-110" id="blog-section">
-    <div className="container">
+  const getStatistics = () => {
+    axiosFetch({
+      axiosInstance: axios,
+      method: "get",
+      url: "/news",
+    });
+  };
+
+  useEffect(() => {
+    getStatistics();
+
+    // eslint-disable-next-line
+  }, []);
+
+  return (
+    <section className="blog-area ptb-110" id="blog-section">
+      <div className="container">
         <div className="section-title">
-            <h2> آخرین اخبار اقتصادی</h2>
+          <h2> آخرین اخبار اقتصادی</h2>
         </div>
 
         <div className="row">
-            <div className="col-lg-6 col-md-12">
-                <div className="single-blog-post mb-0">
-                    <div className="post-image">
-                        <a href=""><img src="" alt=""/></a>
+          {!loading &&
+            !error &&
+            posts.data?.news.map((item, index) => (
+              <div className="col-lg-6 col-md-12" key={index} style={{marginBottom:"20px"}}>
+                <a href={item.url} target="_blank">
+                  <div className="single-blog-post mb-0">
+
+                      <div className="post-image"  style={{borderRadius:"10px 10px 0px 0px"}}>
+                     
+                        <img src={item.image} style={{borderRadius:"10px 10px 0px 0px"}} alt={`news ${index}`} />
+     
+                      </div>
+
+
+                    <div className="post-content" style={{borderRadius:"0px 0px 10px 10px",backgroundColor:"#ececec"}}>
+                      <ul className="post-meta">
+                        <li>
+                          <i className="icofont-tags"></i>{" "}
+                          <p>{item.subtitle}</p>
+                        </li>
+                        <li>
+                          <i className="icofont-calendar"></i>
+                          {item.date}
+                        </li>
+                      </ul>
+
+                      <h3 className="mb-0">
+                        <a href={item.url} target="_blank">{item.title}</a>
+                      </h3>
+                      <p>{item.lead}</p>
                     </div>
-
-                    <div className="post-content">
-                        <ul className="post-meta">
-                            <li><i className="icofont-tags"></i> <a href="">
-                                {/* {{data_get($news,"0.suptitle")}} */}
-                                </a></li>
-                            <li><i className="icofont-calendar"></i>
-                            {/* {{data_get($news,"0.date")}} */}
-                            </li>
-                        </ul>
-
-                        <h3 className="mb-0"><a href="">
-                            {/* {{data_get($news,"0.title")}}  */}
-                            </a></h3>
-                        <p>
-                         {/* {{data_get($news,"0.lead")}} */}
-                        </p>
-
-                    </div>
-                </div>
-            </div>
-
-            <div className="col-lg-6 col-md-12">
-                <div className="blog-item-box">
-                    <div className="blog-item">
-                        <div className="row align-items-center">
-                            <div className="col-lg-4 col-md-3 col-sm-4">
-                                <div className="post-image">
-                                    <a href=""><img src="" alt=""/></a>
-                                </div>
-                            </div>
-
-                            <div className="col-lg-8 col-md-9 col-sm-8">
-                                <div className="post-content">
-                                    <ul className="post-meta">
-                                        <li><i className="icofont-tags"></i> <a href="">
-                                            {/* {{data_get($news,"1.suptitle")}} */}
-                                            </a></li>
-                                        <li><i className="icofont-calendar"></i>
-                                        {/* {{data_get($news,"1.date")}} */}
-                                        </li>
-                                    </ul>
-
-                                    <h3><a href="">
-                                        {/* {{data_get($news,"1.title")}} */}
-                                        </a></h3>
-                                    <p>
-                                        {/* {{data_get($news,"1.lead")}} */}
-                                        </p>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="blog-item">
-                        <div className="row align-items-center">
-                            <div className="col-lg-4 col-md-3 col-sm-4">
-                                <div className="post-image">
-                                    <a href=""><img src="" alt=""/></a>
-                                </div>
-                            </div>
-
-                            <div className="col-lg-8 col-md-9 col-sm-8">
-                                <div className="post-content">
-                                    <ul className="post-meta">
-                                        <li><i className="icofont-tags"></i> <a href="">
-                                            {/* {{data_get($news,"2.suptitle")}} */}
-                                            </a></li>
-                                        <li><i className="icofont-calendar"></i>
-                                        {/* {{data_get($news,"2.date")}} */}
-                                        </li>
-                                    </ul>
-
-                                    <h3><a href="">
-                                        {/* {{data_get($news,"2.title")}} */}
-                                        </a></h3>
-                                    <p>
-                                        {/* {{data_get($news,"2.lead")}} */}
-                                        </p>
-
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className="blog-item">
-                        <div className="row align-items-center">
-                            <div className="col-lg-4 col-md-3 col-sm-4">
-                                <div className="post-image">
-                                    <a href=""><img src="" alt=""/></a>
-                                </div>
-                            </div>
-
-                            <div className="col-lg-8 col-md-9 col-sm-8">
-                                <div className="post-content">
-                                    <ul className="post-meta">
-                                        <li><i className="icofont-tags"></i> <a href="">
-                                            {/* {{data_get($news,"3.suptitle")}} */}
-                                            </a></li>
-                                        <li><i className="icofont-calendar"></i>
-                                        {/* {{data_get($news,"3.date")}} */}
-                                        </li>
-                                    </ul>
-
-                                    <h3><a href="">
-                                        {/* {{data_get($news,"3.title")}} */}
-                                        </a></h3>
-                                    <p>
-                                        {/* {{data_get($news,"3.lead")}} */}
-                                        </p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+                  </div>
+                </a>
+              </div>
+            ))}
         </div>
-    </div>
-</section>
+      </div>
+    </section>
+  );
+};
 
-    )
-}
-
-export default News
+export default News;
